a7f590b7fc930459cae46604255ad97102208326
hung-simon <hung.ha@sotatek.com>
2019-05-07 11:56:59 +0700

    first

---
+++ app/screens/main/MainScreen.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Icon, TabBar } from '@ant-design/react-native';
+import TabBarItem from '@ant-design/react-native/lib/tab-bar/TabBarItem';
+
+export default class MainScreen extends Compoent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+    };
+  }
+
+  renderContent(pageText) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'white' }}>
+        <SearchBar placeholder="Search" showCancelButton />
+        <Text style={{ margin: 50 }}>{pageText}</Text>
+      </View>
+    );
+  }
+
+  onChangeTab(tabName) {
+    this.setState({
+      selectedTab: tabName,
+    });
+  }
+
+  render() {
+    return (
+      <TabBar
+        unselectedTintColor="#949494"
+        tintColor="#33A3F4"
+        barTintColor="#f5f5f5"
+        >
+        <TabBar.Item
+          title="Life"
+          icon={<Icon name="home" />}
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => this.onChangeTab('blueTab')}
+        >
+          {this.renderContent('Life Tab')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Icon name="ordered-list" />}
+          title="Koubei"
+          badge={2}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => this.onChangeTab('redTab')}
+        >
+          {this.renderContent('Koubei Tab')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Icon name="like" />}
+          title="Friend"
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={() => this.onChangeTab('greenTab')}
+        >
+          {this.renderContent('Friend Tab')}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Icon name="user" />}
+          title="My"
+          selected={this.state.selectedTab === 'yellowTab'}
+          onPress={() => this.onChangeTab('yellowTab')}
+        >
+          {this.renderContent('My Tab')}
+        </TabBar.Item>
+      </TabBar>
+    );
+  }
+}
